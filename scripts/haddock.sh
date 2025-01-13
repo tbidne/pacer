739df1de75ab421b8101f0e45a947ee5a0241c38
Tommy Bidne <tbidne@protonmail.com>
2025-01-13 21:09:09 +1300

    Add haddock script

---
+++ scripts/haddock.sh
@@ -0,0 +1,14 @@
+set -e
+
+export LANG="C.UTF-8"
+
+cabal haddock --haddock-hyperlink-source --haddock-quickjump
+
+mkdir -p docs/
+
+# shellcheck disable=SC2038
+fd -t f . docs | xargs -I % sh -c "rm -r %"
+
+dir=$(fd -t d html dist-newstyle/build)
+
+cp -r $dir/pacer/* docs/
