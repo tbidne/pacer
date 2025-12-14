50dcd02b9715d3072f85868e48fc0ec166fad2b2
Tommy Bidne <tbidne@protonmail.com>
2025-12-15 11:24:31 +1300

    Expose full compiler version

--- src/Pacer/Configuration/Args.hs
+++ src/Pacer/Configuration/Args.hs
@@ -154,7 +154,7 @@ versionInfo :: VersionInfo
 versionInfo =
   MkVersionInfo
     { gitCommitDate = d,
-      ghc = showVersion Info.compilerVersion,
+      ghc = showVersion Info.fullCompilerVersion,
       gitHash = h,
       gitShortHash = sh
     }
